5b8122c9b072dc6deda9ee81871cb8ee00be2a0a
Al3xiel <gabriel280104casas@gmail.com>
2024-09-12 16:22:08 -0500

    feat(i18n): setup i18n support.

--- src/main.js
+++ src/main.js
@@ -29,6 +29,7 @@ import Tag from "primevue/tag";
 import Textarea from "primevue/textarea";
 import Toolbar from "primevue/toolbar";
 import Toast from "primevue/toast";
+import i18n from "./i18n.js";
 
 
 const app = createApp(App);
@@ -62,5 +63,7 @@ app.component('pv-button', Button)
     .component('pv-textarea', Textarea)
     .component('pv-toolbar', Toolbar)
     .component('pv-toast', Toast);
+
+app.use(i18n);
 
 app.mount('#app')
