db139832e16ccd6d3c64b1b9f555bb0e889aefc0
Al3xiel <gabriel280104casas@gmail.com>
2024-09-13 16:36:20 -0500

    feat(public): implemented about us component.

--- src/public/pages/about.component.vue
+++ src/public/pages/about.component.vue
@@ -7,7 +7,10 @@ export default {
 <template>
 
 </template>
-
+  <div class="w-full">
+    <h1>About</h1>
+    <p>ACME Learning Center is an Education Business Platform part of ACME Corporation.</p>
+  </div>
 <style scoped>
 
 </style>
