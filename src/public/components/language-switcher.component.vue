db67fc903ac13166f3e9b70c722be3a771403b1f
Al3xiel <gabriel280104casas@gmail.com>
2024-09-13 16:45:02 -0500

    feat(i18n): implemented language switcher component.

---
+++ src/public/components/language-switcher.component.vue
@@ -0,0 +1,31 @@
+<script>
+
+import i18n from "../../i18n.js";
+
+export default {
+  name: "language-switcher",
+  computed: {
+    i18n() {
+      return i18n
+    }
+  },
+  data() {
+    return {
+      languages: ['en','es'],
+      language: 'en'
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-select-button v-model="$i18n.locale" :options="languages">
+    <template #option="slotProps">
+      {{slotProps.option.toUpperCase()}}
+    </template>
+  </pv-select-button>
+</template>
+
+<style scoped>
+
+</style>
